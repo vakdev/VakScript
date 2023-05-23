a1c6f9a2f5fef84ecf8077a3ad04855e6f9fde7f
vakdev <93299015+vakdev@users.noreply.github.com>
2023-05-23 07:56:26 -0600

    Update Autoconfig.py

--- Autoconfig.py
+++ Autoconfig.py
@@ -1,30 +1,38 @@
 #ext
-from mousekey import get_active_window
-from os import path, system
+import os
+import win32api
 import fileinput
+from psutil import process_iter
 
 #own
-from Settings import jsonGetter, jsonSetter
+from Settings import jsonSetter
 from Data import Data
+
+
+DEFAULT_CAMERA_SNAP = '[Space],[v]'
+DEFAULT_CHAMPION_ONLY = '[Button 3]'
+DEFAULT_PLAYER_ATTACK = '[a]'
+DEFAULT_WALK_CLICK = '[Button 2]'
+DEFAULT_RANGE = '[o]'
+DEFAULT_ORBWALK = '[space]'
+DEFAULT_LANECLEAR = '[v]'
+DEFAULT_LASTHIT = '[c]'
 
 options = {
     'EnableTargetedAttackMove':'1',
     'TargetChampionsOnlyAsToggle':'0',
-    'evtCameraSnap':'[Space],[v]',
-    'evtChampionOnly':'[Button 3]',
-    'evtPlayerAttackMoveClick':'[a]',
-    'evtPlayerMoveClick':'[Button 2]',
-    'evtShowCharacterMenu':'[o]'
+    'evtCameraSnap':DEFAULT_CAMERA_SNAP,
+    'evtChampionOnly':DEFAULT_CHAMPION_ONLY,
+    'evtPlayerAttackMoveClick':DEFAULT_PLAYER_ATTACK,
+    'evtPlayerMoveClick':DEFAULT_WALK_CLICK,
+    'evtShowCharacterMenu':DEFAULT_RANGE
 }
 
 class Autoconfig:
     
-    def __init__(self):
-        self.autoconfig = jsonGetter().getSetting("autoconfig")
-        lol_path = get_active_window()[-1]
-        self.settings_to_persist = path.join(lol_path, Data.settings_to_persist_path)
-        self.persisted_settings = path.join(lol_path, Data.persisted_settings_path)
-        system(f'taskkill -f -im "{Data.game_name_executable}"')
+    def __init__(self, settings_to_persist, persisted_settings):
+        self.settings_to_persist = settings_to_persist
+        self.persisted_settings = persisted_settings
 
     @staticmethod
     def clearName(string):
@@ -41,48 +49,62 @@ class Autoconfig:
         with open(self.persisted_settings) as persisted_settings:
             for index, line in enumerate(persisted_settings, 1):
                 line = Autoconfig.clearValue(line)
-                if line in current_values.values() and index not in current_values.keys():
-                    if len(line) > 1:
-                        duplications[index] = line
+                if not line.isdecimal() and '.' not in line:
+                    for k, v in current_values.items():
+                            #remove normal keys
+                            if line == v and index != k:
+                                duplications[index] = line
+                            
+                            #remove double bound keys
+                            if (line.startswith(v) or line.endswith(v)) and index != k:
+                                if not line.startswith(('[Alt]', '[Ctrl]')):
+                                    duplications[index] = line
+                            
+                            #remove C for lasthit
+                            if line == DEFAULT_LASTHIT:
+                                duplications[index] = line
+
+                            #remove V for laneclear
+                            if line == DEFAULT_LANECLEAR:
+                                duplications[index] = line
+
+                            #remove SPACE for orbwalk
+                            if line == DEFAULT_ORBWALK:
+                                duplications[index] = line
 
         for line in fileinput.input(self.persisted_settings, inplace=True):
             current_index = fileinput.lineno()
             for k, v in duplications.items():
                 if current_index == k:
                     line = line.replace(v, '[<Unbound>],[<Unbound>]')
-
             print(line, end="")
 
     def getPersistedSettings(self):
-        if self.autoconfig:
-            current_settings = dict()
-            is_value = False
-            value_pos = None
-            with open(self.persisted_settings, "r+") as persisted_settings:
-                for index, line in enumerate(persisted_settings, 1):
-                    line_name = Autoconfig.clearName(line)
-                    if line_name in options.keys():
-                        value_pos = index + 1
-                        is_value = True
-                        continue
-                    if is_value:
-                        current_settings[value_pos] = Autoconfig.clearValue(line)
-                        is_value = False
-
-            return current_settings
+        current_settings = dict()
+        is_value = False
+        value_pos = None
+        with open(self.persisted_settings, "r+") as persisted_settings:
+            for index, line in enumerate(persisted_settings, 1):
+                line_name = Autoconfig.clearName(line)
+                if line_name in options.keys():
+                    value_pos = index + 1
+                    is_value = True
+                    continue
+                if is_value:
+                    current_settings[value_pos] = Autoconfig.clearValue(line)
+                    is_value = False
+        return current_settings
         
 
     def setPersistedSettings(self):
-        if self.autoconfig:
-            current_settings = self.getPersistedSettings()
-            for line in fileinput.input(self.persisted_settings, inplace=True):
-                current_index = fileinput.lineno()
-                for (value_pos, current_value), new_value in zip(current_settings.items(), options.values()):
-                    if value_pos == current_index:
-                        line = line.replace(current_value, new_value)
-                print(line, end="")
-
-            self.removeDuplications()
+        current_settings = self.getPersistedSettings()
+        for line in fileinput.input(self.persisted_settings, inplace=True):
+            current_index = fileinput.lineno()
+            for (value_pos, current_value), new_value in zip(current_settings.items(), options.values()):
+                if value_pos == current_index:
+                    line = line.replace(current_value, new_value)
+            print(line, end="")
+        self.removeDuplications()
 
     def toggleSettingsToPersist(self, statment):
         for line in fileinput.input(self.settings_to_persist, inplace=True):
@@ -94,11 +116,11 @@ class Autoconfig:
             print(line, end="")
 
     def setJsonSettings(self):
-        jsonSetter().setKey('orbwalk', 'space')
-        jsonSetter().setKey('laneclear', 'v')
-        jsonSetter().setKey('lasthit', 'x')
-        jsonSetter().setMode('orbwalk', 'Lowest Health')
-        jsonSetter().setMode('lasthit', 'auto')
+        jsonSetter().setKey('orbwalk', DEFAULT_ORBWALK.removeprefix('[').removesuffix(']'))
+        jsonSetter().setKey('laneclear', DEFAULT_LANECLEAR.removeprefix('[').removesuffix(']'))
+        jsonSetter().setKey('lasthit', DEFAULT_LASTHIT.removeprefix('[').removesuffix(']'))
+        jsonSetter().setMode('orbwalk', 'Less Basic Attacks')
+        jsonSetter().setMode('lasthit', 'Auto')
         jsonSetter().setSetting('autoconfig', False)
         jsonSetter().setSetting('ppc', False)
         jsonSetter().setSetting('ext_drawings', True)
@@ -107,11 +129,33 @@ class Autoconfig:
         jsonSetter().setSetting('ext_limited', False)
         jsonSetter().setSetting('freeze', False)
 
-
-    def start(self):
+    def set_config(self):
         self.toggleSettingsToPersist(False)
         self.setPersistedSettings()
         self.toggleSettingsToPersist(True)
         self.setJsonSettings()
+        
+def start_autoconfig():
+    league_path = None
+    for process in process_iter(['name', 'cwd']):
+        if process.info['name'] == Data.client_name_executable:
+            os.system('taskkill -f -im "{}"'.format(Data.game_name_executable))
+            league_path = process.info['cwd']
 
+    if league_path is not None and os.path.isdir(league_path):
+        settings_to_persist = os.path.join(league_path, Data.settings_to_persist_path)
+        persisted_settings = os.path.join(league_path, Data.persisted_settings_path)
+        files_list = [settings_to_persist, persisted_settings]
 
+        for file in files_list:
+            #prevent read-only files. (first attempt)
+            try:
+                win32api.SetFileAttributes(file, 128)    
+            except:
+                #second attempt.
+                try:
+                    os.chmod(file, 0o777)
+                except:
+                    pass
+
+        Autoconfig(settings_to_persist, persisted_settings).set_config()
