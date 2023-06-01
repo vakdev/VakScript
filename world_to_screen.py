56cf49c1babfc8d458a264c4abb8a625d612232e
vakdev <93299015+vakdev@users.noreply.github.com>
2023-06-01 08:51:00 -0600

    Update and rename WorldToScreen.py to world_to_screen.py

--- world_to_screen.py
+++ world_to_screen.py
@@ -3,7 +3,7 @@ from numpy import array, frombuffer, float32
 from pyMeow import r_bytes
 
 #own
-from Data import Offsets
+from data import Offsets
 
 class World:
 
@@ -12,12 +12,13 @@ class World:
         self.base_address = base_address
         self.width = width
         self.height = height
+        self.view_proj_matrix = Offsets.view_proj_matrix
 
     def _list_to_matrix(self, floats):
         return array(floats).reshape(4,4)
     
     def get_view_proj_matrix(self):
-        data = r_bytes(self.process, self.base_address + Offsets.oViewProjMatrix, 0x128)
+        data = r_bytes(self.process, self.base_address + self.view_proj_matrix, 0x128)
         view_matrix = self._list_to_matrix(frombuffer(data[:64], dtype=float32))
         proj_matrix = self._list_to_matrix(frombuffer(data[64:128], dtype=float32))
         view_proj_matrix = view_matrix @ proj_matrix
@@ -36,7 +37,7 @@ class World:
 
         return out_x, out_y
 
-    def world_to_screen_restricted(self, view_proj_matrix, x, y, z):
+    def world_to_screen_limited(self, view_proj_matrix, x, y, z):
         clip_coords_x = x * view_proj_matrix[0] + y * view_proj_matrix[4] + z * view_proj_matrix[8] + view_proj_matrix[12]
         clip_coords_y = x * view_proj_matrix[1] + y * view_proj_matrix[5] + z * view_proj_matrix[9] + view_proj_matrix[13]
         clip_coords_w = x * view_proj_matrix[3] + y * view_proj_matrix[7] + z * view_proj_matrix[11] + view_proj_matrix[15]
