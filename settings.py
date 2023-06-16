fbcf703484c6c23fb7fe5c15ef93e7e8704e2032
vakdev <93299015+vakdev@users.noreply.github.com>
2023-06-16 01:59:21 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -11,32 +11,44 @@ class jsonSetter:
         self.file = open(self.file_name, "r+")
         self.settings = load(self.file)
 
-    def _jsonUpdate(self):
+    def _json_update(self):
         self.file.seek(0)
         dump(self.settings, self.file, indent=4)
         self.file.truncate()
         self.file.close()
 
-    def isValidKey(self, key):
-        return key.lower() in VK_CODES.keys()
+    def is_valid_data(self, data):
+        return data.lower() in VK_CODES.keys()
     
-    def setKey(self, id, key):
-        if self.isValidKey(key):
-            self.settings['AutoKite']['Keys'][id] = key.lower()
-            self._jsonUpdate()
-
-    def setMode(self, id, mode):
-        self.settings['AutoKite']['Modes'][id] = mode
-        self._jsonUpdate()
-
-    def setSetting(self, id, setting):
-        self.settings['AutoKite']['Settings'][id] = setting
-        self._jsonUpdate()
-
-    def setSmiteKey(self, id, key):
-        if isinstance(key, str):
-            if self.isValidKey(key):
-                self.settings['AutoSmite']['Keys'][id] = key.lower()
+    def set_spaceglider_data(self, key, data):
+        if isinstance(data, str):
+            if self.is_valid_data(data):
+                self.settings['Spaceglider'][key] = data.lower()
+            elif key.endswith(('mode', 'prio')):
+                self.settings['Spaceglider'][key] = data
         else:
-            self.settings['AutoSmite']['Keys'][id] = key
-        self._jsonUpdate()
+            self.settings['Spaceglider'][key] = data
+        self._json_update()
+
+    def set_drawings_data(self, key, data):
+        self.settings['Drawings'][key] = data
+        self._json_update()
+
+    def set_autosmite_data(self, key, data):
+        if isinstance(data, str):
+            if self.is_valid_data(data):
+                self.settings['AutoSmite'][key] = data.lower()
+        else:
+            self.settings['AutoSmite'][key] = data
+        self._json_update()
+
+class jsonGetter(jsonSetter):
+
+    def __init__(self):
+        super().__init__()
+
+    def get_data(self, key):
+        for sk, sv in self.settings.items():
+            if key in sv:
+                return self.settings[sk][key]
+        self.settings.close()
