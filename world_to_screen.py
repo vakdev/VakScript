d23b9087b4af4f6d05a213dcfadaef1accc6a0ed
vakdev <93299015+vakdev@users.noreply.github.com>
2023-06-28 17:51:32 -0600

    Update world_to_screen.py

--- world_to_screen.py
+++ world_to_screen.py
@@ -1,7 +1,6 @@
 #ext
-from numpy import array, frombuffer, float32
 from pyMeow import r_bytes
-
+from numpy import dot, array, frombuffer, float32
 #own
 from data import Offsets
 
@@ -14,40 +13,24 @@ class World:
         self.height = height
         self.view_proj_matrix = Offsets.view_proj_matrix
 
-    def _list_to_matrix(self, floats):
-        return array(floats).reshape(4,4)
-    
     def get_view_proj_matrix(self):
         data = r_bytes(self.process, self.base_address + self.view_proj_matrix, 0x128)
-        view_matrix = self._list_to_matrix(frombuffer(data[:64], dtype=float32))
-        proj_matrix = self._list_to_matrix(frombuffer(data[64:128], dtype=float32))
-        view_proj_matrix = view_matrix @ proj_matrix
-        return view_proj_matrix.reshape(16)
+        view_matrix = frombuffer(data[:64], dtype=float32).reshape(4, 4)
+        proj_matrix = frombuffer(data[64:128], dtype=float32).reshape(4, 4)
+        view_proj_matrix = dot(view_matrix, proj_matrix)
+        return view_proj_matrix
     
     def world_to_screen(self, view_proj_matrix, x, y, z):
-        clip_coords_x = x * view_proj_matrix[0] + y * view_proj_matrix[4] + z * view_proj_matrix[8] + view_proj_matrix[12]
-        clip_coords_y = x * view_proj_matrix[1] + y * view_proj_matrix[5] + z * view_proj_matrix[9] + view_proj_matrix[13]
-        clip_coords_w = x * view_proj_matrix[3] + y * view_proj_matrix[7] + z * view_proj_matrix[11] + view_proj_matrix[15]
-
-        if clip_coords_w <= 0.:
-            clip_coords_w = .1
-
-        M_x, M_y = clip_coords_x / clip_coords_w, clip_coords_y / clip_coords_w
-        out_x, out_y = (self.width / 2. * M_x) + (M_x + self.width / 2.), -(self.height / 2. * M_y) + (M_y + self.height / 2.) 
-
-        return out_x, out_y
+        clip_coords = dot(array([x, y, z, 1.0]), view_proj_matrix.reshape(4, 4))
+        if clip_coords[3] <= 0.: clip_coords[3] = 0.1
+        clip_coords /= clip_coords[3]
+        return (self.width / 2.0 * clip_coords[0]) + (clip_coords[0] + self.width / 2.0), -(self.height / 2.0 * clip_coords[1]) + (clip_coords[1] + self.height / 2.0)
 
     def world_to_screen_limited(self, view_proj_matrix, x, y, z):
-        clip_coords_x = x * view_proj_matrix[0] + y * view_proj_matrix[4] + z * view_proj_matrix[8] + view_proj_matrix[12]
-        clip_coords_y = x * view_proj_matrix[1] + y * view_proj_matrix[5] + z * view_proj_matrix[9] + view_proj_matrix[13]
-        clip_coords_w = x * view_proj_matrix[3] + y * view_proj_matrix[7] + z * view_proj_matrix[11] + view_proj_matrix[15]
-
-        if clip_coords_w <= 0.:
-            clip_coords_w = .1
-
-        M_x, M_y = clip_coords_x / clip_coords_w, clip_coords_y / clip_coords_w
-        out_x, out_y = (self.width / 2. * M_x) + (M_x + self.width / 2.), -(self.height / 2. * M_y) + (M_y + self.height / 2.)
+        clip_coords = dot(array([x, y, z, 1.0]), view_proj_matrix.reshape(4, 4))
+        if clip_coords[3] <= 0.: clip_coords[3] = 0.1
+        clip_coords /= clip_coords[3]
+        out_x, out_y =  (self.width / 2.0 * clip_coords[0]) + (clip_coords[0] + self.width / 2.0), -(self.height / 2.0 * clip_coords[1]) + (clip_coords[1] + self.height / 2.0)
 
         if 0 <= out_x <= self.width and 0 <= out_y <= self.height:
             return out_x, out_y
-        
