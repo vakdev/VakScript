426973fdd15078df684550a8acba477ac1199a00
vakdev <93299015+vakdev@users.noreply.github.com>
2023-10-27 10:02:58 -0600

    Update drawings.py

--- drawings.py
+++ drawings.py
@@ -1,40 +1,21 @@
-"""
-Modifications in pyMeow source for its use in VakScript:
-
-v13.14 - game / script version.
-
-- Removed MSAA_4X from overlayInit().
-
-- Instead of using overlayLoop, added clearBackground to clear drawings directly. (vakClearBackground) . code:
-
-# proc vakClearBackground {.exportpy: "vak_clear_background".} = 
-#   clearBackground(Blank)
-
-"""
-
-
-#verified context and no warnings.
-import requests
-import ssl
-requests.packages.urllib3.disable_warnings()
-ssl._create_default_https_context = ssl._create_unverified_context
-
-#ext
-from pyMeow import open_process, get_module, load_font, new_color
-from pyMeow import overlay_init, overlay_close, begin_drawing, end_drawing, vak_clear_background
-from pyMeow import draw_line, draw_circle, draw_font, gui_progress_bar, gui_text_box
-from pyMeow import r_uint64
-from win32api import GetSystemMetrics
+#built-in
 from gc import collect as del_mem
 from time import sleep
 from math import cos, sin, pi
 
+#ext
+from pyMeow import open_process, get_module, load_font, new_color
+from pyMeow import overlay_init, overlay_loop, overlay_close, begin_drawing, end_drawing
+from pyMeow import draw_line, draw_circle, draw_font, gui_progress_bar, gui_text_box
+from pyMeow import r_uint64
+from win32api import GetSystemMetrics
+
 #own
-from data import Data, Offsets
-from entities import EntityDrawings, ReadAttributes, distance
+from data import Info, Offsets
+from entities import EntityConditions, TargetSelector, AttributesReader
 from world_to_screen import World
 from settings import jsonGetter
-from utils import safe_title
+from utils import safe_title, debug_info
 
 class Colors:
     Lightgray = new_color(200, 200, 200, 255)
@@ -65,76 +46,100 @@ class Colors:
     Magenta = new_color(255, 0, 255, 255)
     Raywhite = new_color(245, 245, 245, 255)
 
-def drawings(terminate, settings, champion_pointers, turret_pointers, on_window):
+class Draw:
 
-    def draw_enemy_line(own_pos, pos, width, height):
-        x, y = pos[0], pos[1]
-        if (x < 0 or x > width or y < 0 or y > height) and distance(player, entity) <= 3000:
-                draw_line(own_pos[0], own_pos[1], x, y, Colors.Red, 2.)
-        else:
-            draw_line(own_pos[0], own_pos[1], x, y, Colors.Lime, 2.)
-            draw_circle(x, y, 5, Colors.Lime)
+    def __init__(self, world, width, height):
+        self.width = width
+        self.height = height
+        self.world_to_screen = world.world_to_screen
+        self.world_to_screen_limited = world.world_to_screen_limited
 
-    def draw_health(entity, own_pos, pos, width, height):
-        if not (0 <= pos[0] < width and 0 <= pos[1] < height):
-            dx = pos[0] - own_pos[0]
-            dy = pos[1] - own_pos[1]
+        #entity_range
+        self.num_vert = 24 
+        self.angle_increment = 2 * pi / self.num_vert
+        self.cos_values = [cos(i * self.angle_increment) for i in range(self.num_vert)]
+        self.sin_values = [sin(i * self.angle_increment) for i in range(self.num_vert)]
 
-            dx_inv = 1.0 / dx if dx != 0 else float('inf')
-            dy_inv = 1.0 / dy if dy != 0 else float('inf')
+    @staticmethod
+    def spell_level(pos, spell_levels):
+        x_space = 0
+        for level in spell_levels:
+            gui_text_box(pos[0] - 60 + x_space, pos[1] - 125, 35, 25, '', 1)
+            draw_font(1, str(level), pos[0] - 50 + x_space, pos[1] - 120, 18, 0, Colors.Gold)
+            x_space += 35
 
-            t_left = -(own_pos[0] - 30) * dx_inv
-            t_right = (width - own_pos[0] - 130) * dx_inv
-            t_top = -(own_pos[1] - 30) * dy_inv
-            t_bottom = (height - own_pos[1] - 30) * dy_inv
-
-            valid_t_values = [t for t in [t_left, t_right, t_top, t_bottom] if t >= 0]
-
-            if valid_t_values:
-                t_min = min(valid_t_values)
-                x_intersect = own_pos[0] + t_min * dx
-                y_intersect = own_pos[1] + t_min * dy
-                draw_font(1, entity.name, x_intersect, y_intersect - 20, 20, 2, Colors.Gold)
-                gui_progress_bar(x_intersect, y_intersect, 100, 15, "", "", entity.health, 0, entity.max_health)
-
-    #for draw_range
-    num_vert = 24
-    angle_increment = 2 * pi / num_vert
-    cos_values = [cos(i * angle_increment) for i in range(num_vert)]
-    sin_values = [sin(i * angle_increment) for i in range(num_vert)]
-            
-    def draw_range(view_proj_matrix, game_pos, radius, thickness, color):
-        for i in range(num_vert):
-            vec1 = world_to_screen(
-                view_proj_matrix, game_pos[0] + cos_values[i] * radius, game_pos[1], game_pos[2] + sin_values[i] * radius
+    def entity_range(self, view_proj_matrix, game_pos, radius, thickness=1.0, color=Colors.Cyan):
+        for i in range(self.num_vert):
+            vec1 = self.world_to_screen(
+                view_proj_matrix, game_pos[0] + self.cos_values[i] * radius, game_pos[1], game_pos[2] + self.sin_values[i] * radius
             )
-            next_index = (i + 1) % num_vert
-            vec2 = world_to_screen(
-                view_proj_matrix, game_pos[0] + cos_values[next_index] * radius, game_pos[1], game_pos[2] + sin_values[next_index] * radius
+
+            next_index = (i + 1) % self.num_vert
+
+            vec2 = self.world_to_screen(
+                view_proj_matrix, game_pos[0] + self.cos_values[next_index] * radius, game_pos[1], game_pos[2] + self.sin_values[next_index] * radius
             )
 
             draw_line(vec1[0], vec1[1], vec2[0], vec2[1], color, thickness)
+
+    def health(self, entity, own_pos, pos):
+        # Draw healthbars around the screen aiming to enemy position in world.
+
+        if own_pos:
+            if not (0 <= pos[0] < self.width and 0 <= pos[1] < self.height):
+                dx = pos[0] - own_pos[0]
+                dy = pos[1] - own_pos[1]
+
+                dx_inv = 1.0 / dx if dx != 0 else float('inf')
+                dy_inv = 1.0 / dy if dy != 0 else float('inf')
+
+                t_left = -(own_pos[0] - 30) * dx_inv
+                t_right = (self.width - own_pos[0] - 130) * dx_inv
+                t_top = -(own_pos[1] - 30) * dy_inv
+                t_bottom = (self.height - own_pos[1] - 30) * dy_inv
+
+                valid_t_values = [t for t in [t_left, t_right, t_top, t_bottom] if t >= 0]
+
+                if valid_t_values:
+                    t_min = min(valid_t_values)
+                    x_intersect = own_pos[0] + t_min * dx
+                    y_intersect = own_pos[1] + t_min * dy
+                    draw_font(1, entity.name, x_intersect, y_intersect - 20, 20, 2, Colors.Gold)
+                    gui_progress_bar(x_intersect, y_intersect, 100, 15, "", "", entity.health, 0, entity.max_health)
+        
+    def line_to_enemy(self, own_pos, pos, thickness=1.0, color=Colors.Lime):
+        draw_line(own_pos[0], own_pos[1], pos[0], pos[1], color, thickness)
+        draw_circle(pos[0], pos[1], 5.0, color)
+
+
+def drawings(terminate, settings, champion_pointers, turret_pointers, on_window):
+    """ External drawings process."""
 
     while not terminate.value:
         if on_window.value:
             del_mem()
             try:
-                process = open_process(process=Data.game_name_executable)
-                base_address = get_module(process, Data.game_name_executable)['base']
-                local = r_uint64(process, base_address + Offsets.local_player)
-                attr = ReadAttributes(process, base_address)
+                process = open_process(process=Info.game_name_executable)
+                base_address = get_module(process, Info.game_name_executable)['base']
+                local_player = r_uint64(process, base_address + Offsets.local_player)
+                attr_reader = AttributesReader(process, base_address)
 
-                width = GetSystemMetrics(0)
-                height = GetSystemMetrics(1)
-                world = World(process, base_address, width, height)
-                entity = EntityDrawings(process, base_address, width, height)
-                min_attacks = entity.min_attacks
+                screen_width = GetSystemMetrics(0)
+                screen_height = GetSystemMetrics(1)
 
-                #if is_active_window():
+                world = World(process, base_address, screen_width, screen_height)
+                draw = Draw(world, screen_width, screen_height)
+                world_to_screen = world.world_to_screen
+                world_to_screen_limited = world.world_to_screen_limited
+                get_view_proj_matrix = world.get_view_proj_matrix
+                
+
+                target_selector = TargetSelector(world)
+                target_selector.drawings_mode = True    # Ignores if entity is not in player range
+
                 show_position = settings['show_position']
                 show_focused = settings['show_focused']
                 show_healths = settings['show_healths']
-                #show_gold = settings['show_gold']
                 show_spells = settings['show_spells']
                 show_player_range = settings['show_player_range']
                 show_enemy_range = settings['show_enemy_range']
@@ -142,94 +147,83 @@ def drawings(terminate, settings, champion_pointers, turret_pointers, on_window)
                 show_hits = settings['show_hits']
                 screen_track = settings['screen_track']
                 fps = settings['fps']
-
-                #spells_keys = attr.spells_keys
-
                 target_prio = jsonGetter().get_data('orbwalk_prio')
+                
+                target_prio_modes = {
+                    'Less Basic Attacks' : target_selector.select_by_health,
+                    'Most Damage' : target_selector.select_by_damage,
+                    'Nearest Enemy' : target_selector.select_by_distance
+                }
 
-                target_prio_mode = {'Less Basic Attacks':entity.select_by_health,
-                                    'Most Damage':entity.select_by_damage,
-                                    'Nearest Enemy':entity.select_by_distance}
-
-                select_target = target_prio_mode.get(target_prio, entity.select_by_health)
-
-                read_player = attr.read_player
-                read_enemy = attr.read_enemy
-                read_turret = attr.read_turret
-                read_spells = attr.read_spells
-                world_to_screen = world.world_to_screen
-                world_to_screen_limited = world.world_to_screen_limited
-                get_view_proj_matrix = world.get_view_proj_matrix
-
+                select_target = target_prio_modes.get(target_prio, target_selector.select_by_health)
 
                 if screen_track:
                     world_to_screen = world.world_to_screen_limited
 
-            except:
-                continue
-        
+            except Exception as drawings_proc_loop:
+                debug_info(drawings_proc_loop, True)
             else:
                 try:
-                    if not fps.isdecimal():
-                        fps = 0
+                    if not fps.isdecimal() or fps == '0':
+                        fps = 60
+                    
                     overlay_init(title=safe_title(), fps=int(fps))
-                    load_font(Data.font_file_name, 1)
-                
-                    while 1:
-                        begin_drawing()
+                    load_font(Info.font_file_name, 1)
 
-                        entities = [read_enemy(pointer) for pointer in champion_pointers]
-                        player = read_player(local)
+                    while overlay_loop():
+                        entities = [attr_reader.read_enemy(pointer) for pointer in champion_pointers]
+                        player = attr_reader.read_player(local_player)
                         view_proj_matrix = get_view_proj_matrix()
                         own_pos = world_to_screen(view_proj_matrix, player.x, player.z, player.y)
 
+                        begin_drawing()
+
                         if show_player_range:
-                            draw_range(view_proj_matrix, (player.x, player.z, player.y), player.attack_range + 65.0, 4, Colors.Gold)
+                            draw.entity_range(view_proj_matrix, (player.x, player.z, player.y), player.attack_range + 65.0, color=Colors.Orange)
 
                         if show_turret_range:
-                            turrets = [read_turret(pointer) for pointer in turret_pointers]
+                            turrets = [attr_reader.read_turret(pointer) for pointer in turret_pointers]
                             for turret in turrets:
-                                draw_range(view_proj_matrix, (turret.x, turret.z, turret.y), 890.0, 2, Colors.Red)
+                                if EntityConditions.hurtable(turret):
+                                    draw.entity_range(view_proj_matrix, (turret.x, turret.z, turret.y), 890.0, 1.0, Colors.Red)
 
                         for entity in entities:
-                            pos = world_to_screen(view_proj_matrix, entity.x, entity.z, entity.y)
+                            if EntityConditions.hurtable(entity):
+                                pos = world_to_screen(view_proj_matrix, entity.x, entity.z, entity.y)
+                                if pos:
+                                    if show_enemy_range:
+                                        draw.entity_range(view_proj_matrix, (entity.x, entity.z, entity.y), entity.attack_range + 65.0, color=Colors.Cyan)
+                                    
+                                    if show_position:
+                                        draw.line_to_enemy(own_pos, pos, color=Colors.Gray)
 
-                            if entity.alive and entity.visible:
-                                if show_enemy_range:
-                                    draw_range(view_proj_matrix, (entity.x, entity.z, entity.y), entity.attack_range + 65.0, 2, Colors.Cyan)
-                                
-                                if show_position and pos:
-                                    draw_enemy_line(own_pos, pos, width, height)
+                                    if show_hits:
+                                        min_attacks = entity.health / EntityConditions.effective_damage(player.basic_attack + player.bonus_attack, entity.armor)
+                                        draw_font(1, str(round(min_attacks)), pos[0], pos[1] + 50, 20, 2, Colors.Raywhite)
 
-                                if show_hits and pos:
-                                    draw_font(1, str(round(min_attacks(player, entity))), pos[0], pos[1] + 50, 20, 2, Colors.Raywhite)
-                                
-                                if show_healths and pos:
-                                    draw_health(entity, own_pos, pos, width, height)
-                                
-                                if show_spells and pos:
-                                    spells_levels = read_spells(entity.pointer)
-                                    x_space = 0
-                                    for level in spells_levels:
-                                        gui_text_box(pos[0] - 60 + x_space, pos[1] - 125, 35, 25, '', 1)
-                                        draw_font(1, str(level), pos[0] - 50 + x_space, pos[1] - 120, 18, 0, Colors.Gold)
-                                        x_space += 35
+                                    if show_healths:
+                                        draw.health(entity, own_pos, pos)
 
-                            if show_focused:
-                                target = select_target(player, entities)
-                                if target:
-                                    pos_limited = world_to_screen_limited(view_proj_matrix, target.x, target.z, target.y)
-                                    if pos_limited:
-                                        draw_line(own_pos[0], own_pos[1], pos_limited[0], pos_limited[1], Colors.Gold, 3)
-                                        draw_circle(pos_limited[0], pos_limited[1], 8, Colors.Gold)
-                                        draw_range(view_proj_matrix, (target.x, target.z, target.y), target.attack_range + 65.0, 3, Colors.Pink)
+                                    if show_spells:
+                                        spell_levels = attr_reader.read_spells(entity.pointer)
+                                        draw.spell_level(pos, spell_levels)
+
+                        if show_focused:
+                            target = select_target(player, entities)
+                            if target:
+                                pos = world_to_screen_limited(view_proj_matrix, target.x, target.z, target.y)
+                                if pos:
+                                    draw.line_to_enemy(own_pos, pos, 2.0, Colors.Magenta)
+                        
+                        end_drawing()
 
                         if not on_window.value:
                             overlay_close()
                             break
 
-                        end_drawing()
-                        vak_clear_background()
-                except:
+                except Exception as drawings_loop:
+                    debug_info(drawings_loop, True)
                     overlay_close()
-        sleep(.1)
+
+
+
