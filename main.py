b27f0a08c2d619449b583ac7f332c08677d225a8
vakdev <93299015+vakdev@users.noreply.github.com>
2023-10-27 10:04:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,39 @@
-#ext
-from multiprocessing import Process, Manager, freeze_support
-from dearpygui.dearpygui import create_context, destroy_context, create_viewport, setup_dearpygui, show_viewport, is_dearpygui_running, render_dearpygui_frame
-from ctypes import windll 
+
+"""
+[v13.21]:
+    - Auto last hit while spacing removed.
+    - Spell tracker (lvl) removed.
+    - Custom pyMeow.pyd removed.
+"""
+
+#built-in
 import sys
+from multiprocessing import Process, Manager, freeze_support
+from ctypes import windll
 
 #own
 from multiprocessing_functions import MultiprocessingFunctions
-from gui import gui
-from utils import safe_title
-
-def is_admin():
-    try:
-        return windll.shell32.IsUserAnAdmin()
-    except:
-        return False
+from gui import show_gui
+from utils import debug_info
 
 if __name__ == '__main__':
-    freeze_support()
+    freeze_support() #required for pyinstaller --onefile
 
-    if not is_admin():
-        windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+    if not windll.shell32.IsUserAnAdmin():
+        windll.shell32.ShellExecuteW(None, 'runas', sys.executable, __file__, None, 1)
         sys.exit()
 
     manager = Manager()
     main_instance = MultiprocessingFunctions(manager)
 
-    proc = Process(target=main_instance.updater)
-    proc.start()
+    updater_process = Process(target=main_instance.updater)
+    updater_process.start()
+    
+    show_gui(main_instance)
 
-    width, height = 290, 420
-    create_context()
-    gui(main_instance, width, height)
-    create_viewport(
-        title=safe_title(),
-        width=width, height=height,
-        max_width=width, max_height=height,
-        x_pos=0, y_pos=0,
-        resizable=False
-    )
-
-    setup_dearpygui()
-    show_viewport()
-
-    while is_dearpygui_running():
-        render_dearpygui_frame()
-
-    destroy_context()
     main_instance.drawings_terminate.value = 1
     main_instance.autosmite_terminate.value = 1
     main_instance.updater_terminate.value = 1
     main_instance.spaceglider_terminate.value = 1
+
+
