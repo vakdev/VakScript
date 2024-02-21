989954ced4054560df0d0d908fdf2d60153855b0
Shurtug <98952641+Shurtug@users.noreply.github.com>
2024-02-21 12:15:12 +0100

    delete testing file

--- vakscript/from win32api import GetSystemMetrics, G.py
+++
@@ -1,9 +0,0 @@
-from win32api import GetSystemMetrics, GetKeyState, GetCursorPos
-import time
-while True:
-    if (GetKeyState('S') < 0): {
-        print("b")
-    } 
-    else: {
-        time.sleep(0.5)
-    }
